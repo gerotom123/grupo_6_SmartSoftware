581cac4e8d6e994e1a12a8746accaf63c97129ad
MatiMorzilli <matpepino04@gmail.com>
2022-03-06 15:30:50 -0300

    Cambios

--- RETRO.md
+++ RETRO.md
@@ -1,2 +1,6 @@
+SPRINT 2||
 Con respecto al sprint 1 creemos que nos organizamos de una buena manera, aunque con algunos percances que lo retrasaron un poco, pero en general todo bien, llegamos al objetivo deseado así que por el momento muy contentos con el resultado. 
 En cuanto a la división de tareas nos dividimos sin problemas
+ 
+ SPRINT 3||
+ 1. Comenzamos a hacer: 
--- app.js
+++ app.js
@@ -1,40 +1,45 @@
 const express = require('express');
 const app = express();
+const methodOverride = require('method-override');
+app.use(methodOverride('_method'));
 app.use(express.static('public'));
-
+app.set('view engine', 'ejs');
 
 app.listen(3000, ()=>{
     console.log('Servidor funcionando');
 });
 
+app.use(express.urlencoded({extended: false}));
+app.use(express.json())
+
 app.get('/', (req,res)=>{
-    res.sendFile(__dirname + '/views/home.html');
+    res.sendFile(__dirname + '/views/home.ejs');
 });
 app.get('/login', (req,res)=>{
-    res.sendFile(__dirname + '/views/login.html');
+    res.sendFile(__dirname + '/views/login.ejs');
 });
 app.get('/productdetail', (req,res)=>{
-    res.sendFile(__dirname + '/views/productdetail.html');
+    res.sendFile(__dirname + '/views/productdetail.ejs');
 });
 app.get('/carrito', (req,res)=>{
-    res.sendFile(__dirname + '/views/carrito.html');
+    res.sendFile(__dirname + '/views/carrito.ejs');
 });
 app.get('/register', (req,res)=>{
-    res.sendFile(__dirname + '/views/register.html');
+    res.sendFile(__dirname + '/views/register.ejs');
 });app.get('/Black-Camo', (req,res)=>{
-    res.sendFile(__dirname + '/views/Black-Camo.html');
+    res.sendFile(__dirname + '/views/Black-Camo.ejs');
 });
 app.get('/Black&White-Camo', (req,res)=>{
-    res.sendFile(__dirname + '/views/Black&White-Camo.html');
+    res.sendFile(__dirname + '/views/Black&White-Camo.ejs');
 });
 app.get('/Ice-Short', (req,res)=>{
-    res.sendFile(__dirname + '/views/Ice-Short.html');
+    res.sendFile(__dirname + '/views/Ice-Short.ejs');
 });
 app.get('/Monster-Trouser', (req,res)=>{
-    res.sendFile(__dirname + '/views/Monster-Trouser.html');
+    res.sendFile(__dirname + '/views/Monster-Trouser.ejs');
 });
 app.get('/Retro-Shirt', (req,res)=>{
-    res.sendFile(__dirname + '/views/Monster-Trouser.html');
+    res.sendFile(__dirname + '/views/Monster-Trouser.ejs');
 });app.get('/Shark-Jumper', (req,res)=>{
-    res.sendFile(__dirname + '/views/Shark-Jumper.html');
+    res.sendFile(__dirname + '/views/Shark-Jumper.ejs');
 });
