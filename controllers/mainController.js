b6ddbfc7b811a6f6311dc9e7d6937da94af68ec7
Yonibel <gyonibel@gmail.com>
2022-03-06 18:02:03 -0300

    Se crea el controlador y las vistas

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,10 +1,35 @@
 const express = require('express');
 const path = require('path');
 
+const productos = {
+    '1': {
+        'url': '/img/img1',
+        'nombre': 'Black Camo',
+        'precio': 1750,
+        'descripcion': 'pantalon negro, talle L',
+    }  
+}
+
 const controlador = {
     home:(req,res)=>{
        res.render('home');
 },
+    login:(req,res)=>{
+        res.render('login');
+},
+    carrito:(req,res)=>{
+        res.render('carrito');
+},
+    register:(req,res)=>{
+        res.render('register');
+},
+
+    productoDetalle:(req,res)=>{
+        let  productodetalles = req.params.id;
+        res.render('productodetalle', { 
+            'productos':productos[productodetalles]});
+},
+
 }
 
 module.exports = controlador;
--- routes/mainRouter.js
+++ routes/mainRouter.js
@@ -5,6 +5,14 @@ const controlador = require('../controllers/mainController')
 
 router.get('/', controlador.home);
 
+router.get('/login', controlador.login);
+
+router.get('/carrito', controlador.carrito);
+
+router.get('/register', controlador.register);
+
+router.get('/productodetalle', controlador.productoDetalle);
+
 
 
 module.exports = router;
